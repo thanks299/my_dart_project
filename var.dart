2240b70246a56c64ccf61dfc2d65260d3c6b92fc
thanks299 <thanksagbeble@gmail.com>
2024-12-06 00:01:34 +0100

    dart tasks

---
+++ var.dart
@@ -0,0 +1,15 @@
+void main() {
+  String geniusName = 'thanks';    // 🧙‍♂️ A String spell!
+  int iqLevel = 1000;             // 💪 Integer magic!
+  double intellisensePower = 75.5;         // 💥 Double trouble!
+  bool highfaculty = true;        // 🪄 A magical staff? True or False?
+  
+  print('Name: $geniusName');
+  print('iqLevel: $iqLevel');
+  print('intellisensePower: $intellisensePower');
+  print('highfaculty: $highfaculty');
+  
+  // 🔥 Update a variable
+  iqLevel = 1100;
+  print('New iqLevel: $iqLevel');
+}
